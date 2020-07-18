f8b8b1347579eab3e2f32b7e4633ecb02a353cf5
quiet396 <kohei.ingenious3.fx@hotmail.com>
2020-07-18 17:16:01 +0900

    [mod] load ajax action

--- RailsReactApp/app/controllers/data_controller.rb
+++ RailsReactApp/app/controllers/data_controller.rb
@@ -1,5 +1,9 @@
 class DataController < ApplicationController
   def index
+    url = 'https://news.yahoo.co.jp/pickup/rss.xml'
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    render plain:Hash::from_xml(response.body).to_json
   end
 
   def ajax
