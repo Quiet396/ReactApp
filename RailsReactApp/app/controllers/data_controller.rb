9ed2ba6b9731a74d120a7f8301dd963649f09d29
quiet396 <kohei.ingenious3.fx@hotmail.com>
2020-07-18 16:07:34 +0900

    [mod] note controller method

--- RailsReactApp/app/controllers/data_controller.rb
+++ RailsReactApp/app/controllers/data_controller.rb
@@ -3,5 +3,13 @@ class DataController < ApplicationController
   end
 
   def ajax
+    if params[:name] then
+      data = Datum.where 'name like ?', '%' +
+          params[:name] + '%'
+    else
+      data = Datum.all
+    end
+    render plain:data.to_json
   end
+
 end
