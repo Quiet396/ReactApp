3331b6a0fbcc6cd8c973ad7709968e7987dd3a82
quiet396 <kohei.ingenious3.fx@hotmail.com>
2020-07-18 15:46:34 +0900

    [mod] use attribute of component

--- RailsReactApp/app/javascript/packs/hello_react.jsx
+++ RailsReactApp/app/javascript/packs/hello_react.jsx
@@ -1,26 +1,32 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
+const Calc = props => {
+  let n = props.number;
+  let total = 0;
+  for (let i = 0;i <= n;i++){
+    total += i;
+  }
+  return (
+    <div>ゼロから {props.number} までの合計は、「{total} 」です。</div>
+  );
 }
 
-Hello.propTypes = {
-  name: PropTypes.string
+Calc.defaultProps = {
+  number: 0
+}
+
+Calc.propTypes = {
+  number: PropTypes.integer
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
+  let el = (<div>
+    <Calc number="100" />
+    <Calc number="200" />
+    <Calc number="300" />
+  </div>);
+  let dom = document.querySelector('#hello');
+  ReactDOM.render(el, dom);
 })
